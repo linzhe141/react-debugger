ba58ed5686e679fe4733ae737c4f6fd6172eaa7f
linzhe141 <1572213544@qq.com>
2024-12-20 17:50:30 +0800

    update useState init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 
 function App() {
+  debugger
   const [count, setCount] = useState(0)
+  const [num, setNum] = useState(100)
   window.__setCount = setCount
+  window.__setNum = setNum
   return (
     <div>
       <span>{count}</span>
+      <span>{num}</span>
     </div>
   )
 }
